558e30301ae8c2d1c8acaee3dbe7156fa1ae1a40
Theomilll <theo.alb.moura@gmail.com>
2026-03-11 14:37:14 -0300

    add consoleblame formula v0.1.0

--- Formula/consoleblame.rb
+++ Formula/consoleblame.rb
@@ -1,8 +1,9 @@
 class Consoleblame < Formula
   desc "Stream browser console output to your terminal"
   homepage "https://github.com/theomilll/consoleblame"
-  url "https://github.com/theomilll/consoleblame/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9eda0368d430a2458cd72c87ecd9153e230ef7f1d11cfe509bac313f892e3c29"
+  url "https://github.com/theomilll/consoleblame/archive/5ae476a4af6d3d52203123f3c16ffb80cc734d04.tar.gz"
+  version "0.1.0"
+  sha256 "d58d8fbafdf1f750e359e858fb961ed70fe811e3091e5b8cc149f3e335d1ebb8"
   license "MIT"
 
   depends_on "node"
